824c1193304561a118b3bc29b3500afdf8e68b20
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-06-23 12:36:31 +0600

    Completed basic TODO checkbox for practice ts

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ type Task = {
 };
 
 const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById(
-  "#new-task-form"
-) as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>("#new-task-input");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
 
-form?.addEventListener("submit", (element) => {
-  element.preventDefault();
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
 
   if (input?.value == "" || input?.value == null) return;
 
@@ -24,12 +24,34 @@ form?.addEventListener("submit", (element) => {
     completed: false,
     createdAt: new Date(),
   };
+  tasks.push(newTask);
+  saveTasks();
+
   addListItem(newTask);
+  input.value = "";
 });
 
-const addListItem = (task: Task) => {
+function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
   checkbox.type = "checkbox";
-};
+  checkbox.checked = task.completed;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
