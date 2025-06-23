a18e72850242aee091131b04636f3c8d814f79f9
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-06-23 12:16:56 +0600

    create ts project with snowpack and assign input field

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { v4 as uuidV4 } from "uuid";
+
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById(
+  "#new-task-form"
+) as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-input");
+
+form?.addEventListener("submit", (element) => {
+  element.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+  addListItem(newTask);
+});
+
+const addListItem = (task: Task) => {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+};
